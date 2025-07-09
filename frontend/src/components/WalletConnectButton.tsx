bc1f1dd27ab9b7c124086b2f11149859317599be
collezionegrenga <collezionegrenga@gmail.com>
2025-07-09 15:55:57 +0200

    Update WalletConnectButton.tsx

--- frontend/src/components/WalletConnectButton.tsx
+++ frontend/src/components/WalletConnectButton.tsx
@@ -1,38 +1,13 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const WalletConnectButton: React.FC = () => {
   const { t } = useTranslation();
-  const endpoint = useMemo(() => clusterApiUrl("mainnet-beta"), []);
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
-    []
-  );
+  
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <WalletMultiButton className="!bg-blue-600 !text-white" />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <WalletMultiButton className="!bg-blue-600 !text-white" />
   );
 };
 
